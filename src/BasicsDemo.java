7aa0827e859a6e772daa77f6957de9bdf8fab7f1
johnlangmyer <john.langmyer@gmail.com>
2021-03-10 22:02:54 -0600

    Commit message

--- src/BasicsDemo.java
+++ src/BasicsDemo.java
@@ -1,8 +1,19 @@
 public class BasicsDemo {
 
+    public static void primitives(){
+        System.out.println("\n\nInside primitives...");
+        int intHex = 0x0041;
+        int intBinary = 0b0100_0001;
+        int intOctal = 0101;
+        System.out.println("intHex: " + intHex);
+        System.out.println("intBinary: " + intBinary);
+        System.out.println("intBinary: " + intOctal);
+
+    }
     public static void main (String[] args){
         int i = 2;
         int j = 3;
         System.out.println( i + j );
+        primitives();
     }
 }
--- src/Student.java
+++ src/Student.java
@@ -3,11 +3,25 @@ public class Student {
     String name;
     String gender;
 
+    byte age = 18;
+    byte rank = 127; // -128 to +128
+    long phone = 223_456_7890L; // Java 7 onwards for readability
+
+    // Integer literals: int literal, long literal
+
+    int minValue = Integer.MIN_VALUE;
+    int maxValue = Integer.MAX_VALUE;
+
     void compute(){
 
         int nextID = id + 1;
         System.out.println("id: " + id);
         System.out.println("Next id: " + nextID);
+        System.out.println("age: " +  age);
+        System.out.println("Rank: " + rank);
+        System.out.println("Phone: " + phone);
+        System.out.println(minValue);
+        System.out.println(maxValue);
     }
 
     boolean updateProfile(String newName){
@@ -15,7 +29,7 @@ public class Student {
         return true;
     }
 
-    public static void main(String args[]){
+    public static void main(String[] args){
         Student s = new Student();
         s.compute();
     }
