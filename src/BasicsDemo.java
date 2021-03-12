8fb9832ca5873eb518c8cc3f2e523a425ad44ab0
johnlangmyer <john.langmyer@gmail.com>
2021-03-11 22:17:56 -0600

    Lectures 29-35

--- src/BasicsDemo.java
+++ src/BasicsDemo.java
@@ -1,3 +1,5 @@
+import java.math.BigDecimal;
+
 public class BasicsDemo {
 
     public static void primitives(){
@@ -5,15 +7,41 @@ public class BasicsDemo {
         int intHex = 0x0041;
         int intBinary = 0b0100_0001;
         int intOctal = 0101;
+
+        char charInt = 65;
+        char charHex = 0x0041;
+        char charBinary = 0b0100_0001;
+
+
         System.out.println("intHex: " + intHex);
         System.out.println("intBinary: " + intBinary);
-        System.out.println("intBinary: " + intOctal);
+        System.out.println("intOctal: " + intOctal);
+        System.out.println("charInt: " + charInt);
+        System.out.println("charHex: " + charHex);
+        System.out.println("charBinary: " + charBinary);
 
     }
     public static void main (String[] args){
-        int i = 2;
-        int j = 3;
-        System.out.println( i + j );
+
+        // int i = 2;
+        // int j = 3;
+        // System.out.println( i + j );
         primitives();
+
+        System.out.println("\nDecimal examinations:");
+        System.out.println(1-0.7); // floating point binary has trouble
+        // 0.1 ~ 0.00110011001100110011.....
+
+        double price = 1000;
+        double discountPercent = 0.9;
+        double discountAmount = price* discountPercent;
+        System.out.println(discountAmount);
+        System.out.println(1 - discountPercent);
+
+        BigDecimal first = new BigDecimal("0.1");
+        BigDecimal second = new BigDecimal("0.2");
+
+        System.out.println(first.add(second)); // Use big decimal from math package - these are objects now, not numbers
+
     }
 }
--- src/Student.java
+++ src/Student.java
@@ -1,27 +1,44 @@
 public class Student {
-    int id = 1000;
-    String name;
-    String gender;
+    int id;
+    String name, gender;
+    byte age, rank; // -128 to +127
+    long phone;
+    double gpa;
+    char degree;
+    boolean international;
 
-    byte age = 18;
-    byte rank = 127; // -128 to +128
-    long phone = 223_456_7890L; // Java 7 onwards for readability
+    static int computeCount;
+
+    double tuitionFees = 100.0;
+    double internationalFees = tuitionFees = 5000.0;
 
     // Integer literals: int literal, long literal
 
-    int minValue = Integer.MIN_VALUE;
-    int maxValue = Integer.MAX_VALUE;
+    //int minValue = Integer.MIN_VALUE;
+    //int maxValue = Integer.MAX_VALUE;
 
     void compute(){
+        computeCount++;
 
         int nextID = id + 1;
-        System.out.println("id: " + id);
+
+        if (international){
+            tuitionFees += internationalFees;
+        }
+
+        System.out.println("\nid: " + id);
         System.out.println("Next id: " + nextID);
+        System.out.println("name: " + name);
+        System.out.println("gender: " + gender);
         System.out.println("age: " +  age);
         System.out.println("Rank: " + rank);
         System.out.println("Phone: " + phone);
-        System.out.println(minValue);
-        System.out.println(maxValue);
+        //System.out.println(minValue);
+        //System.out.println(maxValue);
+        System.out.println("gpa: " + gpa);
+        System.out.println("degree: " + degree);
+        System.out.println("tuition fees: " + tuitionFees);
+        System.out.println("Compute count: " + computeCount);
     }
 
     boolean updateProfile(String newName){
@@ -30,7 +47,43 @@ public class Student {
     }
 
     public static void main(String[] args){
-        Student s = new Student();
-        s.compute();
+        Student student1  = new Student();
+
+        student1.id = 1000;
+        student1.name = "John";
+        student1.gender = "male";
+        student1.age = 18;
+        student1.rank = 127; // -128 to +127
+        student1.phone = 223_456_7890L; // Java 7 onwards for readability
+        student1.gpa = 3.8;
+        student1.degree = '\u0041';
+        student1.international = true;
+        student1.compute();
+
+        Student student2  = new Student();
+        student2.id = 1001;
+        student2.name = "Raj";
+        student2.gender = "male";
+        student2.age = 21;
+        student2.rank = 126; // -128 to +127
+        student2.phone = 414_654_0987L; // Java 7 onwards for readability
+        student2.gpa = 3.2;
+        student2.degree = '\u0042';
+        student2.international = false;
+        student2.compute();
+
+        Student student3 = new Student();
+        student3.id = 1002;
+        student3.name = "Anita";
+        student3.gender = "female";
+        student3.age = 20;
+        student3.phone = 223_456_8888L;
+        student3.gpa = 4.0;
+        student3.degree = 'M';
+        student3.international = true;
+        student3.compute();
+
+        System.out.println("\nStudent.computeCount: " + Student.computeCount);
+
     }
 }
